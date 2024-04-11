bfba21543a09b7ec976c103af5e8dd7ec51930b6
NemanjaCyber <neca.s@elfak.rs>
2024-04-11 19:04:54 +0200

    Add project files.

---
+++ LogRegApp/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using LogRegApp.Models;
+using System.Security.Cryptography;
+using System.Text;
+using LogRegApp.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogRegApp.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")]
+    [ApiController]
+    public class AccountController:ControllerBase
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if(await UserExists(registerDto.Username))
+            {
+                return BadRequest("Username already exists!");
+            }
+
+            var hmac = new HMACSHA256();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key,
+            };
+
+            _context.AppUsers.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.AppUsers.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>>Login(LoginDto loginDto)
+        {
+            var user = await _context.AppUsers
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null)
+            {
+                return BadRequest("Invalid username");
+            }
+
+            var hmac=new HMACSHA256(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i=0;i<computedHash.Length;i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return BadRequest("Invalid password");
+                }
+            }
+
+            return Ok(user);
+        }
+    }
+}
---
+++ LogRegApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LogRegApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<AppUser>? AppUsers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
